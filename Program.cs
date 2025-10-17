bd0605d70a35c7146074c44a4cb9893fd6358b9a
ambid17 <ambid17@gmail.com>
2025-10-17 14:56:11 -0500

    add env var tester

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace linuxTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Console.WriteLine($"Environment: {environmentName}");
+        }
+    }
+}
